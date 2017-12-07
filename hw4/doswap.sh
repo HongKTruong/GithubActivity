20eeff3ff35cb5b5cdfc5fba75954ec6fee60d62
Hong Truong <hong.truongk@gmail.com>
2017-12-07 11:44:41 -0800

    Change web1 container name to remove the ecs189_web1_1 stuff

--- hw4/doswap.sh
+++ hw4/doswap.sh
@@ -23,20 +23,16 @@ function swap {
 	 	killitif web2
 	  docker run -itd --name=web2 -p:8080 $1
 	  docker network connect ecs189_default web2
-	 	docker exec ecs189_proxy_1 /bin/bash /bin/swap2.sh
-	 	killitif ecs189_web1_1
+	 	sleep 5 && docker exec ecs189_proxy_1 /bin/bash /bin/swap2.sh
 	 	killitif web1
 	elif grep --quiet $w2 /tmp/yy_xx$$
 	then
-		killitif ecs189_web1_1
 	 	killitif web1
 	  docker run -itd --name=web1 -p:8080 $1
 	  docker network connect ecs189_default web1
-	 	docker exec ecs189_proxy_1 /bin/bash /bin/swap1.sh
-	 	killitif ecs189_web2_1
+	 	sleep 5 && docker exec ecs189_proxy_1 /bin/bash /bin/swap1.sh
 	 	killitif web2
 	fi
 }
 
-swap $1
 swap $1
